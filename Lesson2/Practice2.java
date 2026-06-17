c155b3581ff8c017df1df212c4ba9b2349355ca7
Rina-trabajo99 <rinakoba.trabajo@gmail.com>
2026-06-18 00:57:41 +0900

    upd: javaの練習問題をフォルダ管理構成に修正・レッスン2の内容を実践

---
+++ Lesson2/Practice2.java
@@ -0,0 +1,21 @@
+package Lesson2;
+
+public class Practice2 {
+    public static void main(String[] args) {
+
+    //改行する際は"⧵n"を使用する
+    System.out.println("こんにちは!\nJavaを学んでいます。\n今日も頑張ろう!");
+
+    //タブを使用する際は"⧵t"を使用する
+    System.out.println("名前\t年齢\n太郎\t20\n花子\t22");
+
+    //文字列内にダブルクォーテーションを入れる
+    System.out.println("テキストの途中で\"を出すことができます。");
+
+    //バックスラッシュ文字
+    System.out.println("ディレクトリを記述するときは、バックスラッシュ\"\\\"を使用します。");
+
+
+
+    }
+}
